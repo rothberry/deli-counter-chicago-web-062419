badba2965d5f43ae1c66c633d9db9d607cafcf6d
Phil Roth <phil.roth077@gmail.com>
2019-06-06 19:53:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,6 +6,14 @@ def take_a_number(arr, name)
   puts "Welcome, #{name}. You are number #{arr.length} in line."
 end
 
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+  end
+end
+
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
