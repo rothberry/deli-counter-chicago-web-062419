c212d11e037b0b7c1249dbaabd7368c831cd5de8
Phil Roth <phil.roth077@gmail.com>
2019-06-06 19:46:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,15 +7,22 @@ def take_a_number(arr, name)
 end
 
 # def line(arr)
-#   if arr.length == 0
-#     puts "The line is currently empty."
-#   else
-#     print "The line is currently:"
-#     arr.each do |x|
-#       print ""
-#
-#     end
-#     end
-#   end
-#
-# end
+  if arr.length == 0
+    puts "The line is currently empty."
+  else
+    print "The line is currently:"
+    arr.each do |x|
+      print ""
+
+    end
+    end
+  end
+
+end
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  end
+  puts "People are here"
+end
