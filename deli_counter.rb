bc1e60d0109c54556a1789ce35b36d6ba558eebf
Phil Roth <phil.roth077@gmail.com>
2019-06-06 20:05:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,15 +15,15 @@ def now_serving(katz_deli)
   end
 end
 
-def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
-    message - "The line is currently: "
-
-    katz_deli.each_with)index(1) do |name, i|
-      message << "#{i}. #{name}"
-    end
-    puts message
-  end
-end
+# def line(katz_deli)
+#   if katz_deli.length == 0
+#     puts "The line is currently empty."
+#   else
+#     message - "The line is currently: "
+#
+#     katz_deli.each_with)index(1) do |name, i|
+#       message << "#{i}. #{name}"
+#     end
+#     puts message
+#   end
+# end
