2380249768d4859165ddf48467603beb8bf8b006
Phil Roth <phil.roth077@gmail.com>
2019-06-06 19:35:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,20 @@
 # Write your code here.
 katz_deli = []
 
-def line
-  if
+def take_a_number(arr, str)
 
 end
+
+# def line(arr)
+#   if arr.length == 0
+#     puts "The line is currently empty."
+#   else
+#     print "The line is currently:"
+#     arr.each do |x|
+#       print ""
+#
+#     end
+#     end
+#   end
+#
+# end
